66791f30a46db8a61d536f36008bdda989bc10d5
F1FOFTY <139736876+F1FOFTY@users.noreply.github.com>
2024-06-09 00:55:28 -0400

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,30 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'F1FOFTY'
+description 'A script for QB-Core that allows players to flash their badge and manage badge information.'
+version '1.0.0'
+
+shared_script 'config.lua'
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server.lua'
+}
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/script.js'
+}
+
+dependencies {
+    'qb-core',
+    'oxmysql'
+}
